ddc8bbccf062a3273c72ab03be29788162ba273d
Albarra Naufala <albarraerdanto@gmail.com>
2023-10-17 12:13:37 +0700

    feat: added variants dummy data in customers users and buyers page

--- src/app/(dashboard)/customers/buyers/page.tsx
+++ src/app/(dashboard)/customers/buyers/page.tsx
@@ -5,14 +5,64 @@ import React from "react"
 import { Button, Pagination, Title } from "@/components/atomics"
 import { FunnelIcon, SortAscendingIcon } from "@/assets/icons"
 
+/**
+ * =======================
+ * Dummy Data - Buyers
+ * =======================
+ */
+const buyersData = [
+  {
+    name: "Samanta Legend",
+    email: "samanta@mail.com",
+    phoneNumber: "(307) 555-0133",
+    address: "2972 Westheimer Rd. Santa Ana, Illinois 85486",
+    lastTransaction: "May 31, 2015"
+  },
+  {
+    name: "Jenny Wilson",
+    email: "jenny@mail.com",
+    phoneNumber: "(205) 555-0100",
+    address: "2715 Ash Dr. San Jose, South Dakota 83475",
+    lastTransaction: "October 25, 2019"
+  },
+  {
+    name: "Ronald Richards",
+    email: "rrichards@mail.com",
+    phoneNumber: "(207) 555-0119",
+    address: "8502 Preston Rd. Inglewood, Maine 98380",
+    lastTransaction: "May 20, 2015"
+  },
+  {
+    name: "Annette Black",
+    email: "blackann@mail.com",
+    phoneNumber: "(252) 555-0126",
+    address: "6391 Elgin St. Celina, Delaware 10299",
+    lastTransaction: "August 24, 2013"
+  },
+  {
+    name: "Kristin Watson",
+    email: "kristinw@mail.com",
+    phoneNumber: "(217) 555-0113",
+    address: "8502 Preston Rd. Inglewood, Maine 98380",
+    lastTransaction: "September 9, 2013"
+  },
+  {
+    name: "Jacob Jones",
+    email: "jjones2@mail.com",
+    phoneNumber: "(702) 555-0122",
+    address: "3891 Ranchview Dr. Richardson, California 62639",
+    lastTransaction: "August 2, 2013"
+  }
+]
+
 const DBCustomersBuyers = () => {
   return (
-    <div className='relative space-y-6 p-6'>
-      <h1 className='text-heading-sm font-semibold'>Buyers</h1>
+    <div className='relative p-6 space-y-6'>
+      <h1 className='font-semibold text-heading-sm'>Buyers</h1>
 
-      <section className='relative rounded-lg-10 bg-white p-6'>
+      <section className='relative p-6 bg-white rounded-lg-10'>
         {/* Navigation */}
-        <nav className='mb-8 flex items-center justify-between'>
+        <nav className='flex items-center justify-between mb-8'>
           <Title size='lg' variant='default'>
             Buyers
           </Title>
@@ -20,11 +70,11 @@ const DBCustomersBuyers = () => {
           <div className='flex flex-row gap-3'>
             <Button size='md' variant='default-bg'>
               Sort
-              <SortAscendingIcon className='h-4 w-4 stroke-2' />
+              <SortAscendingIcon className='w-4 h-4 stroke-2' />
             </Button>
             <Button size='md' variant='default-bg'>
               Filter
-              <FunnelIcon className='h-4 w-4 stroke-2' />
+              <FunnelIcon className='w-4 h-4 stroke-2' />
             </Button>
           </div>
         </nav>
@@ -32,75 +82,75 @@ const DBCustomersBuyers = () => {
         {/* Table */}
         <div className='mb-6 overflow-x-auto'>
           <table className='w-full table-auto'>
-            <thead className='bg-netral-15 text-body-sm font-semibold uppercase'>
+            <thead className='font-semibold uppercase bg-netral-15 text-body-sm'>
               <tr>
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>Name</span>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>Name</span>
                 </th>
 
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>
                     Email Adress
                   </span>
                 </th>
 
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>
                     Phone Number
                   </span>
                 </th>
 
-                <th className='w-56 whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>
+                <th className='w-56 px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>
                     Complete Adress
                   </span>
                 </th>
 
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>
                     Last Transaction
                   </span>
                 </th>
 
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>Action</span>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>Action</span>
                 </th>
               </tr>
             </thead>
-            <tbody className='divide-y divide-netral-20 pt-4 text-sm'>
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
-                <tr key={item}>
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className='text-body-base font-medium text-netral-80'>
-                      Samanta Legend
+            <tbody className='pt-4 text-sm divide-y divide-netral-20'>
+              {buyersData.map((item) => (
+                <tr key={item.email}>
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                    <span className='font-medium text-body-base text-netral-80'>
+                      {item.name}
                     </span>
                   </td>
 
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className='text-body-base font-medium text-netral-80'>
-                      samanta@mail.com
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                    <span className='font-medium text-body-base text-netral-80'>
+                      {item.email}
                     </span>
                   </td>
 
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className='text-body-base font-medium text-netral-80'>
-                      (307) 555-0133
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                    <span className='font-medium text-body-base text-netral-80'>
+                      {item.phoneNumber}
                     </span>
                   </td>
 
-                  <td className='w-56 whitespace-pre-wrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className=' whitespace-pre-wrap break-words text-body-base font-medium text-netral-80'>
-                      2972 Westheimer Rd. Santa Ana, Illinois 85486
+                  <td className='w-56 px-3 py-5 text-left whitespace-pre-wrap first:pl-5 last:pr-5'>
+                    <span className='font-medium break-words whitespace-pre-wrap  text-body-base text-netral-80'>
+                      {item.address}
                     </span>
                   </td>
 
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className='text-body-base font-medium text-netral-80'>
-                      October 25, 2019
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                    <span className='font-medium text-body-base text-netral-80'>
+                      {item.lastTransaction}
                     </span>
                   </td>
 
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
                     <Button
                       size='md'
                       variant='primary-nude'
--- src/app/(dashboard)/customers/users/page.tsx
+++ src/app/(dashboard)/customers/users/page.tsx
@@ -12,6 +12,51 @@ import {
 
 import { FunnelIcon, SortAscendingIcon } from "@/assets/icons"
 
+const usersData = [
+  {
+    name: "Samanta Legend",
+    email: "samanta@mail.com",
+    address: "2972 Westheimer Rd. Santa Ana, Illinois 85486",
+    createAt: "Orange",
+    date: "May 6, 2012"
+  },
+  {
+    name: "Annette Black",
+    email: "annette1@mail.com",
+    address: "3517 W. Gray St. Utica, Pennsylvania 57867",
+    createAt: "Toledo",
+    date: "April 28, 2016"
+  },
+  {
+    name: "Dianne Russell",
+    email: "rdianne@mail.com",
+    address: "8502 Preston Rd. Inglewood, Maine 98380",
+    createAt: "Naperville",
+    date: "November 16, 2014"
+  },
+  {
+    name: "Devon Lane",
+    email: "delane@mail.com",
+    address: "2464 Royal Ln. Mesa, New Jersey 45463",
+    createAt: "Fairfield",
+    date: "March 23, 2013"
+  },
+  {
+    name: "Marvin McKinney",
+    email: "marvin5@mail.com",
+    address: "3891 Ranchview Dr. Richardson, California 62639",
+    createAt: "Austin",
+    date: "November 16, 2014"
+  },
+  {
+    name: "Jerome Bell",
+    email: "belljer@mail.com",
+    address: "8502 Preston Rd. Inglewood, Maine 98380",
+    createAt: "Orange",
+    date: "March 23, 2013"
+  }
+]
+
 const DBCustomersUsers = () => {
   //----------------------------------------------------------------------------------//
   const [active, setActive] = React.useState(false)
@@ -20,11 +65,11 @@ const DBCustomersUsers = () => {
   //----------------------------------------------------------------------------------//
 
   return (
-    <div className='relative space-y-6 p-6'>
-      <h1 className='text-heading-sm font-semibold'>Users</h1>
+    <div className='relative p-6 space-y-6'>
+      <h1 className='font-semibold text-heading-sm'>Users</h1>
 
-      <section className='relative rounded-lg-10 bg-white p-6'>
-        <nav className='mb-8 flex items-center justify-between'>
+      <section className='relative p-6 bg-white rounded-lg-10'>
+        <nav className='flex items-center justify-between mb-8'>
           <Title size='lg' variant='default'>
             Users
           </Title>
@@ -44,80 +89,80 @@ const DBCustomersUsers = () => {
 
         <div className='mb-6 overflow-x-auto'>
           <table className='w-full table-auto'>
-            <thead className='bg-netral-15 text-body-sm font-semibold uppercase'>
+            <thead className='font-semibold uppercase bg-netral-15 text-body-sm'>
               <tr>
-                <th className='w-px whitespace-nowrap px-3 py-4 first:pl-5 last:pr-5'>
+                <th className='w-px px-3 py-4 whitespace-nowrap first:pl-5 last:pr-5'>
                   <Checkbox active={active} setActive={setActive} />
                 </th>
 
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>Name</span>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>Name</span>
                 </th>
 
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>
                     Email Adress
                   </span>
                 </th>
 
-                <th className='w-56 whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>
+                <th className='w-56 px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>
                     Complete Adress
                   </span>
                 </th>
 
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>Created At</span>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>Created At</span>
                 </th>
 
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>
                     Last Activity
                   </span>
                 </th>
 
-                <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
-                  <span className='text-body-sm font-semibold'>Action</span>
+                <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
+                  <span className='font-semibold text-body-sm'>Action</span>
                 </th>
               </tr>
             </thead>
-            <tbody className='divide-y divide-netral-20 pt-4 text-sm'>
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
-                <tr key={item}>
-                  <td className='w-px whitespace-nowrap px-3 py-5 first:pl-5 last:pr-5'>
+            <tbody className='pt-4 text-sm divide-y divide-netral-20'>
+              {usersData.map((item) => (
+                <tr key={item.email}>
+                  <td className='w-px px-3 py-5 whitespace-nowrap first:pl-5 last:pr-5'>
                     <Checkbox active={active} setActive={setActive} />
                   </td>
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className='text-body-base font-medium text-netral-80'>
-                      Samanta Legend
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                    <span className='font-medium text-body-base text-netral-80'>
+                      {item.name}
                     </span>
                   </td>
 
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className='text-body-base font-medium text-netral-80'>
-                      samanta@mail.com
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                    <span className='font-medium text-body-base text-netral-80'>
+                      {item.email}
                     </span>
                   </td>
 
-                  <td className='w-56 whitespace-pre-wrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className=' whitespace-pre-wrap break-words text-body-base font-medium text-netral-80'>
-                      2972 Westheimer Rd. Santa Ana, Illinois 85486
+                  <td className='w-56 px-3 py-5 text-left whitespace-pre-wrap first:pl-5 last:pr-5'>
+                    <span className='font-medium break-words whitespace-pre-wrap text-body-base text-netral-80'>
+                      {item.address}
                     </span>
                   </td>
 
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className='text-body-base font-medium text-netral-80'>
-                      Sep 19, 2010
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                    <span className='font-medium text-body-base text-netral-80'>
+                      {item.createAt}
                     </span>
                   </td>
 
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
-                    <span className='text-body-base font-medium text-netral-80'>
-                      May 6, 2012
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                    <span className='font-medium text-body-base text-netral-80'>
+                      {item.date}
                     </span>
                   </td>
 
-                  <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
+                  <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
                     <Button
                       size='md'
                       variant='primary-nude'
@@ -153,14 +198,14 @@ const DBCustomersUsers = () => {
         className='max-w-lg'
         setOpen={setOpenModalDelete}
       >
-        <main className='mb-10 mt-4'>
+        <main className='mt-4 mb-10'>
           <p className='text-body-base text-netral-80'>
             Are you sure want to delete this user? User which already deleted
             can not be recovered.
           </p>
         </main>
 
-        <footer className='flex w-full justify-end gap-3'>
+        <footer className='flex justify-end w-full gap-3'>
           <Button
             size='md'
             variant='default-nude'
