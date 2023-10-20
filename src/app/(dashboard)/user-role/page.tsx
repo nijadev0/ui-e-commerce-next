56970848d059da3264b0bd228bafe4a10d60f1c4
Albarra Naufala <albarraerdanto@gmail.com>
2023-10-20 12:45:55 +0700

    fix: fixed checkbox in user role

--- src/app/(dashboard)/user-role/page.tsx
+++ src/app/(dashboard)/user-role/page.tsx
@@ -14,58 +14,73 @@ import {
 
 import { CheckIcon, MagnifyingGlassIcon, UserPlusIcon } from "@/assets/icons"
 
-/**
- * =======================
- * Dummy Data - Users
- * =======================
- */
-const usersData = [
-  {
-    name: "Samanta Legend",
-    email: "samanta@mail.com",
-    role: "Super Admin",
-    phone: "(603) 555-0123",
-    date: "May 6, 2012"
-  },
-  {
-    name: "Annette Black",
-    email: "annette1@mail.com",
-    role: "Admin",
-    phone: "(239) 555-0108",
-    date: "April 28, 2016"
-  },
-  {
-    name: "Dianne Russell",
-    email: "rdianne@mail.com",
-    role: "Cashier",
-    phone: "(208) 555-0112",
-    date: "November 16, 2014"
-  },
-  {
-    name: "Devon Lane",
-    email: "delane@mail.com",
-    role: "Admin",
-    phone: "(219) 555-0114",
-    date: "March 23, 2013"
-  },
-  {
-    name: "Marvin McKinney",
-    email: "marvin5@mail.com",
-    role: "Cashier",
-    phone: "(208) 555-0112",
-    date: "November 16, 2014"
-  },
-  {
-    name: "Jerome Bell",
-    email: "belljer@mail.com",
-    role: "Cashier",
-    phone: "(704) 555-0127",
-    date: "March 23, 2013"
-  }
-]
-
 const DBUserRole = () => {
-  const [active, setActive] = React.useState(false)
+  const [listData, setListData] = React.useState([
+    {
+      name: "Samanta Legend",
+      email: "samanta@mail.com",
+      role: "Super Admin",
+      phone: "(603) 555-0123",
+      date: "May 6, 2012",
+      checked: false
+    },
+    {
+      name: "Annette Black",
+      email: "annette1@mail.com",
+      role: "Admin",
+      phone: "(239) 555-0108",
+      date: "April 28, 2016",
+      checked: false
+    },
+    {
+      name: "Dianne Russell",
+      email: "rdianne@mail.com",
+      role: "Cashier",
+      phone: "(208) 555-0112",
+      date: "November 16, 2014",
+      checked: false
+    },
+    {
+      name: "Devon Lane",
+      email: "delane@mail.com",
+      role: "Admin",
+      phone: "(219) 555-0114",
+      date: "March 23, 2013",
+      checked: false
+    },
+    {
+      name: "Marvin McKinney",
+      email: "marvin5@mail.com",
+      role: "Cashier",
+      phone: "(208) 555-0112",
+      date: "November 16, 2014",
+      checked: false
+    },
+    {
+      name: "Jerome Bell",
+      email: "belljer@mail.com",
+      role: "Cashier",
+      phone: "(704) 555-0127",
+      date: "March 23, 2013",
+      checked: false
+    }
+  ])
+  const checkItem = (index: number, checked: boolean) => {
+    const newListData = [...listData]
+    newListData[index].checked = checked
+    setListData(newListData)
+  }
+  const isSelectAll = React.useMemo(
+    () => listData.filter((item) => !item.checked).length === 0,
+    [listData]
+  )
+  const setIsSelectAll = (newIsSelectAll: boolean) => {
+    setListData(listData.map((item) => ({ ...item, checked: newIsSelectAll })))
+  }
+  const isSelecting = React.useMemo(
+    () => listData.filter((item) => item.checked).length > 0,
+    [listData]
+  )
   const [openModalDelete, setOpenModalDelete] = React.useState(false)
   const [openAlertsDelete, setOpenAlertsDelete] = React.useState(false)
 
@@ -102,7 +117,7 @@ const DBUserRole = () => {
             <thead className='bg-netral-15 text-body-sm font-semibold uppercase'>
               <tr>
                 <th className='w-px whitespace-nowrap px-3 py-4 first:pl-5 last:pr-5'>
-                  <Checkbox active={active} setActive={setActive} />
+                  <Checkbox active={isSelectAll} setActive={setIsSelectAll} />
                 </th>
 
                 <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
@@ -131,10 +146,13 @@ const DBUserRole = () => {
               </tr>
             </thead>
             <tbody className='divide-y divide-netral-20 pt-4 text-sm'>
-              {usersData.map((item) => (
+              {listData.map((item, index) => (
                 <tr key={item.name}>
                   <td className='w-px whitespace-nowrap px-3 py-5 first:pl-5 last:pr-5'>
-                    <Checkbox active={active} setActive={setActive} />
+                    <Checkbox
+                      active={item.checked}
+                      setActive={(value: boolean) => checkItem(index, value)}
+                    />
                   </td>
                   <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
                     <span className='text-body-base font-medium text-netral-80'>
@@ -175,7 +193,7 @@ const DBUserRole = () => {
         <Pagination />
 
         {/* Page Action */}
-        {active && (
+        {isSelecting && (
           <PageAction
             actionLabel='Last saved'
             actionDesc='Nov 9, 2022-17.09'
