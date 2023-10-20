421e0561b960f29abf5903fb7da94dc6cad5f268
Albarra Naufala <albarraerdanto@gmail.com>
2023-10-20 11:15:42 +0700

    fix: fixed checkbox in products > category

--- src/app/(dashboard)/products/categories/page.tsx
+++ src/app/(dashboard)/products/categories/page.tsx
@@ -16,29 +16,51 @@ import {
   SquaresFourIcon
 } from "@/assets/icons"
 
-const listCategoriesData = [
-  {
-    categoryImage: "/categories/categories-1.png"
-  },
-  {
-    categoryImage: "/categories/categories-2.png"
-  },
-  {
-    categoryImage: "/categories/categories-3.png"
-  },
-  {
-    categoryImage: "/categories/categories-4.png"
-  },
-  {
-    categoryImage: "/categories/categories-5.png"
-  }
-]
-
 const DBCategories = () => {
   // ------------------------------------------------------------------------------ //
   const [isEmpty, setIsEmpty] = React.useState(true)
   // ------------------------------------------------------------------------------ //
-  const [active, setActive] = React.useState(false)
+  const [listCategoriesData, setListCategoriesData] = React.useState([
+    {
+      categoryImage: "/categories/categories-1.png",
+      checked: false
+    },
+    {
+      categoryImage: "/categories/categories-2.png",
+      checked: false
+    },
+    {
+      categoryImage: "/categories/categories-3.png",
+      checked: false
+    },
+    {
+      categoryImage: "/categories/categories-4.png",
+      checked: false
+    },
+    {
+      categoryImage: "/categories/categories-5.png",
+      checked: false
+    }
+  ])
+  // ------------------------------------------------------------------------------ //
+  const checkItem = (index: number, checked: boolean) => {
+    const newListCategoriesData = [...listCategoriesData]
+    newListCategoriesData[index].checked = checked
+    setListCategoriesData(newListCategoriesData)
+  }
+  const isSelectAll = React.useMemo(
+    () => listCategoriesData.filter((item) => !item.checked).length === 0,
+    [listCategoriesData]
+  )
+  const setIsSelectAll = (newIsSelectAll: boolean) => {
+    setListCategoriesData(
+      listCategoriesData.map((item) => ({ ...item, checked: newIsSelectAll }))
+    )
+  }
+  const isSelecting = React.useMemo(
+    () => listCategoriesData.filter((item) => item.checked).length > 0,
+    [listCategoriesData]
+  )
   // ------------------------------------------------------------------------------ //
   const [openModalDelete, setOpenModalDelete] = React.useState(false)
   const [openModalDraft, setOpenModalDraft] = React.useState(false)
@@ -47,10 +69,10 @@ const DBCategories = () => {
   const [openAlertsDraft, setOpenAlertsDraft] = React.useState(false)
   // ------------------------------------------------------------------------------ //
   return (
-    <div className='relative min-h-screen p-6 space-y-6'>
-      <h1 className='font-semibold text-heading-sm'>Banner</h1>
+    <div className='relative min-h-screen space-y-6 p-6'>
+      <h1 className='text-heading-sm font-semibold'>Banner</h1>
 
-      <section className='relative p-6 space-y-6 bg-white rounded-lg-10'>
+      <section className='relative space-y-6 rounded-lg-10 bg-white p-6'>
         <Tab.Group>
           {/* Navigation */}
           <nav className='space-y-6'>
@@ -85,7 +107,7 @@ const DBCategories = () => {
                       "rounded-lg-10 border p-2 outline-0 transition-all duration-300 ease-out hover:bg-netral-20 ui-selected:border-primary-border ui-selected:bg-primary-surface ui-selected:text-primary-main ui-not-selected:border-netral-30 ui-not-selected:text-netral-100"
                     }
                   >
-                    <SquaresFourIcon className='w-6 h-6' />
+                    <SquaresFourIcon className='h-6 w-6' />
                   </Tab>
 
                   <Tab
@@ -93,7 +115,7 @@ const DBCategories = () => {
                       "rounded-lg-10 border p-2 outline-0 transition-all duration-300 ease-out hover:bg-netral-20 ui-selected:border-primary-border ui-selected:bg-primary-surface ui-selected:text-primary-main ui-not-selected:border-netral-30 ui-not-selected:text-netral-100"
                     }
                   >
-                    <ListIcon className='w-6 h-6' />
+                    <ListIcon className='h-6 w-6' />
                   </Tab>
                 </div>
               </Tab.List>
@@ -104,27 +126,30 @@ const DBCategories = () => {
             {/* Card Version */}
             <Tab.Panel>
               <div className='grid grid-cols-4 gap-5'>
-                {listCategoriesData.map((item) => (
+                {listCategoriesData.map((item, index) => (
                   <figure
                     key={item.categoryImage}
                     className='relative w-full space-y-3'
                   >
-                    <div className='absolute z-20 left-3 top-6'>
-                      <Checkbox active={active} setActive={setActive} />
+                    <div className='absolute left-3 top-6 z-20'>
+                      <Checkbox
+                        active={item.checked}
+                        setActive={(value: boolean) => checkItem(index, value)}
+                      />
                     </div>
 
-                    <div className='relative flex items-center justify-center w-full h-40 overflow-hidden rounded-lg-10 bg-netral-15'>
+                    <div className='relative flex h-40 w-full items-center justify-center overflow-hidden rounded-lg-10 bg-netral-15'>
                       <Link
                         href={"/products/categories/detail"}
-                        className='absolute z-10 flex items-center justify-center w-full h-full transition-all duration-500 ease-out opacity-0 bg-black/25 hover:opacity-100'
+                        className='absolute z-10 flex h-full w-full items-center justify-center bg-black/25 opacity-0 transition-all duration-500 ease-out hover:opacity-100'
                       >
-                        <button className='flex items-center gap-2 p-2 px-4 font-semibold text-white transition-all duration-300 ease-out border-2 border-white rounded-lg hover:bg-white/25'>
+                        <button className='flex items-center gap-2 rounded-lg border-2 border-white p-2 px-4 font-semibold text-white transition-all duration-300 ease-out hover:bg-white/25'>
                           Detail
-                          <ArrowRightIcon className='w-5 h-5 text-white stroke-2' />
+                          <ArrowRightIcon className='h-5 w-5 stroke-2 text-white' />
                         </button>
                       </Link>
 
-                      <div className='relative h-32 aspect-square 2xl:h-40'>
+                      <div className='relative aspect-square h-32 2xl:h-40'>
                         <Image
                           src={item.categoryImage}
                           alt='Categories 1'
@@ -135,7 +160,7 @@ const DBCategories = () => {
                     </div>
 
                     <figcaption className='space-y-1.5'>
-                      <h5 className='font-semibold text-body-xl'>Outer</h5>
+                      <h5 className='text-body-xl font-semibold'>Outer</h5>
                       <p className='line-clamp-2 text-body-sm text-netral-50'>
                         Discovery a variety of outers to keep yourself warm with
                         stylish and comfortable ways.
@@ -151,64 +176,72 @@ const DBCategories = () => {
               {/* Table */}
               <div className='mb-6 overflow-x-auto'>
                 <table className='w-full table-auto'>
-                  <thead className='font-semibold uppercase bg-netral-15 text-body-sm'>
+                  <thead className='bg-netral-15 text-body-sm font-semibold uppercase'>
                     <tr>
-                      <th className='w-px px-3 py-4 whitespace-nowrap first:pl-5 last:pr-5'>
-                        <Checkbox active={active} setActive={setActive} />
+                      <th className='w-px whitespace-nowrap px-3 py-4 first:pl-5 last:pr-5'>
+                        <Checkbox
+                          active={isSelectAll}
+                          setActive={setIsSelectAll}
+                        />
                       </th>
 
-                      <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
-                        <span className='font-semibold text-body-sm'>
+                      <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
+                        <span className='text-body-sm font-semibold'>
                           Category
                         </span>
                       </th>
 
-                      <th className='px-3 py-4 pr-20 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
-                        <span className='font-semibold text-body-sm'>
+                      <th className='whitespace-nowrap px-3 py-4 pr-20 text-left text-netral-50 first:pl-5 last:pr-5'>
+                        <span className='text-body-sm font-semibold'>
                           Description
                         </span>
                       </th>
 
-                      <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
-                        <span className='font-semibold text-body-sm'>
+                      <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
+                        <span className='text-body-sm font-semibold'>
                           Status
                         </span>
                       </th>
 
-                      <th className='px-3 py-4 text-left whitespace-nowrap text-netral-50 first:pl-5 last:pr-5'>
-                        <span className='font-semibold text-body-sm'>
+                      <th className='whitespace-nowrap px-3 py-4 text-left text-netral-50 first:pl-5 last:pr-5'>
+                        <span className='text-body-sm font-semibold'>
                           Action
                         </span>
                       </th>
                     </tr>
                   </thead>
-                  <tbody className='pt-4 text-sm divide-y divide-netral-20'>
-                    {listCategoriesData.map((item) => (
+                  <tbody className='divide-y divide-netral-20 pt-4 text-sm'>
+                    {listCategoriesData.map((item, index) => (
                       <tr key={item.categoryImage}>
-                        <td className='w-px px-3 py-5 whitespace-nowrap first:pl-5 last:pr-5'>
-                          <Checkbox active={active} setActive={setActive} />
+                        <td className='w-px whitespace-nowrap px-3 py-5 first:pl-5 last:pr-5'>
+                          <Checkbox
+                            active={item.checked}
+                            setActive={(value: boolean) =>
+                              checkItem(index, value)
+                            }
+                          />
                         </td>
 
-                        <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                        <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
                           <div className='flex items-center gap-3'>
-                            <div className='relative w-20 h-20 overflow-hidden rounded-lg-10'>
+                            <div className='relative h-20 w-20 overflow-hidden rounded-lg-10'>
                               <Image
                                 src={item.categoryImage}
-                                className='object-cover w-full h-full'
+                                className='h-full w-full object-cover'
                                 alt='Products'
                                 fill
                               />
                             </div>
 
-                            <span className='w-48 font-medium break-words whitespace-pre-wrap text-body-base text-netral-80'>
+                            <span className='w-48 whitespace-pre-wrap break-words text-body-base font-medium text-netral-80'>
                               {"T-Men's UA Storm Armour Down 2.0 Jacket"}
                             </span>
                           </div>
                         </td>
 
-                        <td className='px-3 py-5 pr-20 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                        <td className='whitespace-nowrap px-3 py-5 pr-20 text-left first:pl-5 last:pr-5'>
                           <div className='w-72'>
-                            <span className='font-medium break-words whitespace-pre-wrap w-72 text-body-base text-netral-80'>
+                            <span className='w-72 whitespace-pre-wrap break-words text-body-base font-medium text-netral-80'>
                               Discover easy and casual t-shirt for women and men
                               with variety of colors, pattern and comfy
                               materials.
@@ -216,16 +249,16 @@ const DBCategories = () => {
                           </div>
                         </td>
 
-                        <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
-                          <span className='font-medium text-body-base text-netral-80'>
+                        <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
+                          <span className='text-body-base font-medium text-netral-80'>
                             <Badge variant='success'>Active</Badge>
                           </span>
                         </td>
 
-                        <td className='px-3 py-5 text-left whitespace-nowrap first:pl-5 last:pr-5'>
+                        <td className='whitespace-nowrap px-3 py-5 text-left first:pl-5 last:pr-5'>
                           <Link
                             href={"/products/list-products/detail"}
-                            className='font-medium text-body-base text-primary-main'
+                            className='text-body-base font-medium text-primary-main'
                           >
                             Detail
                           </Link>
@@ -241,7 +274,7 @@ const DBCategories = () => {
       </section>
 
       {/* Page Action */}
-      {active && (
+      {isSelecting && (
         <PageAction
           variant='sticky'
           actionLabel='2 Product Selected'
@@ -261,14 +294,14 @@ const DBCategories = () => {
         className='max-w-lg'
         setOpen={setOpenModalDelete}
       >
-        <main className='mt-4 mb-10'>
+        <main className='mb-10 mt-4'>
           <p className='text-body-base text-netral-80'>
             Are you sure want to delete this category? Category which already
             deleted can not be recovered.
           </p>
         </main>
 
-        <footer className='flex justify-end w-full gap-3'>
+        <footer className='flex w-full justify-end gap-3'>
           <Button
             size='md'
             variant='default-nude'
@@ -304,13 +337,13 @@ const DBCategories = () => {
         className='max-w-lg'
         setOpen={setOpenModalDraft}
       >
-        <main className='mt-4 mb-10'>
+        <main className='mb-10 mt-4'>
           <p className='text-body-base text-netral-80'>
             Are you sure want to draft this category?{" "}
           </p>
         </main>
 
-        <footer className='flex justify-end w-full gap-3'>
+        <footer className='flex w-full justify-end gap-3'>
           <Button
             size='md'
             variant='default-nude'
